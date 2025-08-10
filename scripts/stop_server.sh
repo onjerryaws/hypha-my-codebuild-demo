fe11aa1e0d24fc60930286f94571f90cf4fe61a7
onjerryaws <awspersonal001@gmail.com>
2025-08-10 17:55:16 +0530

    All required files added.

---
+++ scripts/stop_server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This script is intentionally left blank for this simple example.
+# In a real-world scenario, you would stop your application server here.
+# systemctl stop nginx
+# We will let CodeDeploy overwrite the files while NGINX is running.
